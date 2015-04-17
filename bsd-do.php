19f10487aac68864d68431d3384e8b18ffb04766
Abu Jafar Al Farhad <dolar.rp@gmail.com>
2015-04-17 13:15:28 +0600

    1st commit

---
+++ bsd-do.php
@@ -0,0 +1,14 @@
+<?php
+$password="365277";
+
+$link = mysqli_connect("localhost",
+    "dolar",
+    "123456",
+    "word_list");
+
+$query = "INSERT INTO `words`(`id`,`english`,`bangla`)
+          VALUES('','$_POST[english]','$_POST[bangla]')";
+
+mysqli_query($link,$query);
+header('location:bsd.php');
+?>
---
+++ fuckupig.php
@@ -0,0 +1,31 @@
+<?php include("protect.php"); ?>
+<?php
+echo "This information needs to be secured. Members area. Only members should be able to access it";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Dolar's Word List</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bs/jquery-ui.css">
+  <link rel="stylesheet" href="bs/bootstrap.min.css">
+  <script src="bs/jquery.min.js"></script>
+  <script src="bs/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="bs/jquery-ui.js"></script>
+</head>
+<body>
+
+// <?php
+// $link = mysqli_connect("mysql8.000webhost.com",
+//     "a9383500_words",
+//     "words.host22.com",
+//     "a9383500_words");
+
+// $query = "DELETE FROM `words_all`";
+
+// mysqli_query($link,$query);
+// header('location:bsd.php');
+// ?>
